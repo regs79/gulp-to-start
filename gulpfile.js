506dc1bc6e072dbe1068d820b300fd401ca9f919
Craig Regnier <regs79@gmail.com>
2016-11-15 11:19:36 -0500

    Added gulp task to process and minify javascript.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,26 @@
 var	gulp = require('gulp');
-  browserSync = require('browser-sync').create();
-  reload = browserSync.reload;
-	sass = require('gulp-sass');
-	normalize = require('node-normalize-scss').includePaths;
-	neat = require('node-neat').includePaths;
-  cssnano = require('gulp-cssnano');
-  imagemin = require('gulp-imagemin');
-  runSequence = require('run-sequence');
-  nunjucksRender = require('gulp-nunjucks-render');
+    browserSync = require('browser-sync').create();
+    reload = browserSync.reload;
+    sass = require('gulp-sass');
+    normalize = require('node-normalize-scss').includePaths;
+    neat = require('node-neat').includePaths;
+    cssnano = require('gulp-cssnano');
+    imagemin = require('gulp-imagemin');
+    runSequence = require('run-sequence');
+    nunjucksRender = require('gulp-nunjucks-render');
+    uglify = require('gulp-uglify');
+    pump = require('pump');
 
 gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: 'dist/'
-    },
-  })
+    browserSync.init({
+        server: {
+            baseDir: 'dist/'
+        },
+    })
 })
 
 gulp.task('sass', function() {
-  return gulp.src('app/scss/*.scss')
+    return gulp.src('app/scss/*.scss')
   	.pipe(sass({
   		includePaths: [].concat(normalize, neat),
   		outputStyle: 'compressed'
@@ -26,38 +28,44 @@ gulp.task('sass', function() {
     .pipe(cssnano())
   	.pipe(gulp.dest('dist/css'))
     .pipe(browserSync.reload({
-      stream: true
+        stream: true
     }))
 })
 
 gulp.task('images', function(){
-  return gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
-  .pipe(cache(imagemin()))
-  .pipe(gulp.dest('dist/images'))
+    return gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
+    .pipe(cache(imagemin()))
+    .pipe(gulp.dest('dist/images'))
+})
+
+gulp.task('js' ,function(){
+    pump([
+        gulp.src('app/js/**/*.js'),
+        uglify(),
+        gulp.dest('dist/js')
+    ])
 })
 
 gulp.task('nunjucks', function () {
-  // Gets .html and .nunjucks files in pages
-  return gulp.src('app/pages/**/*.+(html|nunjucks)')
-  // Renders template with nunjucks
-  .pipe(nunjucksRender({
-      path: ['app/templates']
+    // Gets .html and .nunjucks files in pages
+    return gulp.src('app/pages/**/*.+(html|nunjucks)')
+    // Renders template with nunjucks
+    .pipe(nunjucksRender({
+        path: ['app/templates']
     }))
-  // output files in app folder
-  .pipe(gulp.dest('dist'))
+    // output files in app folder
+    .pipe(gulp.dest('dist'))
 })
 
 gulp.task('watch', ['browserSync', 'sass', 'nunjucks', 'images'], function(){
-  gulp.watch('app/scss/**/*.scss', ['sass']);
-  gulp.watch('app/images/**/*.+(png|jpg|gif|svg)', ['images']);
-  gulp.watch('app/pages/**/*.nunjucks', ['nunjucks']);
-  gulp.watch('app/templates/**/*.nunjucks', ['nunjucks']);
-  gulp.watch("dist/*.html").on('change', reload);
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    gulp.watch('app/images/**/*.+(png|jpg|gif|svg)', ['images']);
+    gulp.watch('app/js/**/*.js', ['js']);
+    gulp.watch('app/pages/**/*.nunjucks', ['nunjucks']);
+    gulp.watch('app/templates/**/*.nunjucks', ['nunjucks']);
+    gulp.watch("dist/*.html").on('change', reload);
 })
 
 gulp.task('build', function (callback) {
-  runSequence('sass', 
-    ['nunjucks', 'images'],
-    callback
-  )
+    runSequence('sass', ['nunjucks', 'images', 'js'], callback)
 })
